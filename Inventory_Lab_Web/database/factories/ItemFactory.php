95a17c89a3d95e63061d4faa9b93f41bf8c7ea68
Almeriztama <penttagramgd@gmail.com>
2022-03-31 14:59:15 +0700

    fix table item

---
+++ Inventory_Lab_Web/database/factories/ItemFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Item;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ItemFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Item::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        // $faker = \Faker\Factory::create('id_ID'); // more information : https://github.com/fzaninotto/Faker
+        return [
+            'name' => "Computer",
+            'description' => "hahah hehehhooo",
+            'stored_location' => "lemari besi",
+            'category' => "eletronik",
+            'quantity' => 5,
+            'image_path' => 'computer.jpg',
+        ];
+    }
+}
